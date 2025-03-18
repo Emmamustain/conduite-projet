b2d8a64749482d035a613c78059916266f021fe2
nguyenngo <nguyenngohoai98@gmail.com>
2025-03-18 17:21:52 +0100

    config: github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  basePath: '/conduite-projet',
 };
 
 export default nextConfig;
